49117c11ec513ee26c382ea11357238d858c674b
Rafael Sousa Pereira <rafael.sousa.pereira01@gmail.com>
2026-05-18 18:54:03 -0300

    feat: add user relationship in subscription entity

--- app/src/main/java/com/payflow/app/data/local/entity/Subscription.kt
+++ app/src/main/java/com/payflow/app/data/local/entity/Subscription.kt
@@ -1,9 +1,22 @@
 package com.payflow.app.data.local.entity
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
+import com.payflow.app.domain.model.User
 
-@Entity(tableName = "subscriptions")
+@Entity(
+    tableName = "subscriptions",
+    foreignKeys = [
+        ForeignKey(
+            entity = User::class,
+            parentColumns = ["id"],
+            childColumns = ["usuario_id"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
 data class Subscription(
 
     @PrimaryKey(autoGenerate = true)
@@ -14,5 +27,11 @@ data class Subscription(
     val dataInicio: String,
     val dataFim: String,
     val formaPagamento: String,
-    val categoria: String
+    val categoria: String,
+
+    @ColumnInfo(name = "usuario_id")
+    val usuarioId: String,
+
+    val dataCriacao: String,
+    val dataAtualizacao: String,
 )
